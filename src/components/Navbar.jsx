754d966c46cac8e0205d146e1cf69703cbc5f9d5
alifhossinsajjad <https://github.com/alifhossinsajjad>
2025-07-09 00:19:28 +0600

    create onclick menu button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
 import { assets, menuLinks } from '../assets/assets'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
-        const location = useLocation()
-        const [open, setOpen] = useState(false)
+    const location = useLocation()
+    const [open, setOpen] = useState(false)
+    const navigate = useNavigate()
 
     return (
         <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" && "bg-light"}`}>
@@ -16,9 +17,9 @@ const Navbar = () => {
             </Link>
 
             <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${open ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}>
-                {menuLinks.map((link, index) =>(
+                {menuLinks.map((link, index) => (
                     <Link key={index} to={link.path}>
-                    {link.name}
+                        {link.name}
                     </Link>
                 ))}
 
@@ -28,15 +29,19 @@ const Navbar = () => {
                     <img src={assets.search_icon} alt="search" />
                 </div>
 
-                <div  className="flex max-sm:flex-col items-start sm:items-center gap-6">
-                    <button className="cursor-pointer">
+                <div className="flex max-sm:flex-col items-start sm:items-center gap-6">
+                    <button onClick={()=> navigate('/owner')} className="cursor-pointer">
                         Dashboard
                     </button>
-                    <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">
+                    <button onClick={()=> setShowLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">
                         Log in
                     </button>
                 </div>
             </div>
+
+            <button className='sm:hidden cursor-pointer' aria-lable='Menu' onClick={()=> setOpen(!open)}>
+                <img src= {open ? assets.close_icon : assets.menu_icon} alt="menu" />
+            </button>
 
         </div>
     )
