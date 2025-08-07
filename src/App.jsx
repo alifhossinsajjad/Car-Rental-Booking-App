fd69d50fbd8cd80307ee1fc9672259c9d3bd5035
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-07 22:03:32 +0600

    route the all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/home';
+import CarDetails from './pages/CarDetails';
+import Cars from './pages/Cars';
+import MyBookings from './pages/MyBookings';
+
 
 const App = () => {
 
@@ -11,6 +16,16 @@ const App = () => {
   return (
     <>
     {!isOwnerpath && <Navbar setShowLogin={setShowLogin} />}
+
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/car-details/:id' element={<CarDetails/>}/>
+      <Route path='/cars' element={<Cars/>}/>
+      <Route path='/my-bookings' element={<MyBookings/>}/>
+    </Routes>
+
+
+
     </>
   )
 }
