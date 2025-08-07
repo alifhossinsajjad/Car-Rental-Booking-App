eb4686a31dbc690fa84c8eb084ef68dae4a51480
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-07 21:37:15 +0600

    whene owner path section are selected then navbar has gone

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerpath = useLocation().pathname.startsWith('/owner')
+
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+    {!isOwnerpath && <Navbar setShowLogin={setShowLogin} />}
+    </>
   )
 }
 
