fec8f5ad1892ed9c93a69e9c9762926e6f86f64c
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-09 22:56:38 +0600

    fix the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import { useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import CarDetails from './pages/CarDetails';
 import Cars from './pages/Cars';
 import MyBookings from './pages/MyBookings';
