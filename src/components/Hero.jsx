7383c9971a37b494a304b4f58ef573a86ea5176a
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-14 13:53:30 +0600

    create choose the location feature

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, cityList } from '../assets/assets'
 
 const Hero = () => {
   return (
@@ -8,7 +8,19 @@ const Hero = () => {
             Luxury cars on Rent
         </h1>
 
-        <form></form>
+        <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0, 0,0,0.1)]'>
+          <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
+              <div>
+                <select required>
+                  <option value="">PickUp Location</option>
+                  {cityList.map((city)=> <option key={city} value={city}>{city}</option> )}
+                </select>
+                <p className='px-1 text-sm text-gray-500'>
+                  Please Select Location
+                </p>
+              </div>
+          </div>
+        </form>
 
         <img src={assets.main_car} alt="car" className='max-h-74' />
     </div>
