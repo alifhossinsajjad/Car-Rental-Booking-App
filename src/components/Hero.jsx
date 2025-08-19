2043289165eee97d4ca5f9e97134d630dbdf578f
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-19 10:16:36 +0600

    use the picuplocation function and usestate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { assets, cityList } from '../assets/assets'
+import { useState } from 'react'
 
 const Hero = () => {
+
+  const [pickupLocation, setPickupLocation] = useState ('')
+
   return (
     <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
         <h1 className='text-4xl md:text-5xl font-semibold'>
@@ -11,12 +15,12 @@ const Hero = () => {
         <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0, 0,0,0.1)]'>
           <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
               <div>
-                <select required>
+                <select required value={pickupLocation} onChange={(e)=>setPickupLocation(e.target.value)}>
                   <option value="">PickUp Location</option>
                   {cityList.map((city)=> <option key={city} value={city}>{city}</option> )}
                 </select>
-                <p className='px-1 text-sm text-gray-500'>
-                  Please Select Location
+                <p className='px-1 text-sm text-gray-500'>{pickupLocation ? pickupLocation : 'Please Select Location'}
+                  
                 </p>
               </div>
           </div>
