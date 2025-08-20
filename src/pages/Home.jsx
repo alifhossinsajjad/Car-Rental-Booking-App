14e8d32af0d581337d64d34308896223d463b7d1
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-20 19:27:36 +0600

    review section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeatureSection from '../components/FeatureSection'
 import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
     <Hero />
     <FeatureSection/>
     <Banner />
+    <Testimonial/>
     </>
   )
 }
