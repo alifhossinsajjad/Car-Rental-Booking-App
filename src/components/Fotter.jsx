5ca2115f5b3bf4d2dffcb8718fa8170a65dce959
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-20 12:51:04 +0600

    change the footer ui section and added the web link in fotter section

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -54,7 +54,7 @@ const Fotter = () => {
             </div>
             <hr className='border-gray-300 mt-8' />
             <div className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
-                <p>© {new Date().getFullYear()} <a href="https://prebuiltui.com">PrebuiltUI</a>. All rights reserved.</p>
+                <p>© {new Date().getFullYear()} <a href="https://car-rental-booking-app-six.vercel.app/">CarRental.</a>. All rights reserved.</p>
                 <ul className='flex items-center gap-4'>
                     <li><a href="#">Terms</a></li>
                     <li><a href="#">Privacy</a></li>
