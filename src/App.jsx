e757fdda3e4f2453d604a7bef5c72b78d0a9d949
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-25 21:03:59 +0600

    added the book now form  for everyeach car

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import { useState } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './pages/Home';
-import CarDetails from './pages/CarDetails';
-import Cars from './pages/Cars';
-import MyBookings from './pages/MyBookings';
-import Fotter from './components/Fotter';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import CarDetails from "./pages/CarDetails";
+import Cars from "./pages/Cars";
+import MyBookings from "./pages/MyBookings";
+import Fotter from "./components/Fotter";
 
 const App = () => {
-
-  const [showLogin, setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
   // console.log(showLogin);
-  const isOwnerpath = useLocation().pathname.startsWith('/owner')
+  const isOwnerpath = useLocation().pathname.startsWith("/owner");
 
   return (
     <>
-    {!isOwnerpath && <Navbar setShowLogin={setShowLogin} />}
+      {!isOwnerpath && <Navbar setShowLogin={setShowLogin} />}
 
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/car-details/:id' element={<CarDetails/>}/>
-      <Route path='/cars' element={<Cars/>}/>
-      <Route path='/my-bookings' element={<MyBookings/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+      </Routes>
 
-
-    {!isOwnerpath && <Fotter />}
+      {!isOwnerpath && <Fotter />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
