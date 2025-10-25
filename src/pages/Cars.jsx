b8848c7bd1fcbc2e446acb27983205a99a84e61e
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-25 21:48:54 +0600

    complete the input field with usestate

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import { useState } from "react";
 
 const Cars = () => {
-  return (
-    <div>Cars</div>
-  )
-}
+  const [input, setInput] = useState("");
 
-export default Cars
+  return (
+    <div>
+      <div className="flex flex-col items-center py-20 bg-light max-md:px-4">
+        <Title
+          title={"Acailable Cars"}
+          subTitle={
+            "Browse our selection of premium vehicles available for your next adventure "
+          }
+        />
+
+        <div className="flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow">
+          <img src={assets.search_icon} alt="" className="w-4.5 h-4.5 mr-2" />
+
+          <input
+            onClick={(e) => {
+              setInput(e.target.value);
+            }}
+            value={input}
+            type="text"
+            placeholder="search by make , model , or features"
+            className="w-full h-full outline-none text-gray-500"
+          />
+
+          <img src={assets.filter_icon} alt="" className="w-4.5 h-4.5 ml-2" />
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Cars;
